fd9b21cd3ee745f324ea460991bcf59481362239
eetar1 <ekwan14@gmail.com>
2019-04-14 22:46:01 -0700

    Finished heap sort

--- Java/HeapSort.java
+++ Java/HeapSort.java
@@ -1,31 +1,89 @@
 import java.util.ArrayList;
-
+import java.io.*;
 class HeapSort{
 
-    ArrayList<Integer> heap;
+    static int heap[];
+    static int size;
 
-    void heapify(ArrayList<Integer> values){
+    static void heapify(ArrayList<Integer> values){
+       
+        int r =0;
         for(Integer i : values){
-            heap.add(i);
+            size++;
+            heap[r++] = i.intValue();
             bubbleUp();
+            
         }
+
+    }
+    static void insert(int value){
+        heap[size++] = value;
+        bubbleUp(); 
     }
 
-    void bubbleUp(){
+    static void bubbleUp(){
 
-        for(int i = heap.size()-1;i>0;i /=2){
-            if( heap.get(i) > head.get(i/2)){
-                
+        for(int i = size-1;i>-1;i--){
+            if( heap[i] < heap[i/2]){
+                int tmp = heap[i/2];
+                heap[i/2] =heap[i];
+                heap[i] = tmp;
             }
         }
     }
     
-    public void main(String args[]){
+    static int removeMin(){
+
+        int out = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+
+        bubbleDown(0);
+
+        return out;
+    }
+
+    static void bubbleDown(int idx){
+        int leftIdx = idx*2+1;
+        int rightIdx = idx*2+2;
+        int minIdx;
+        
+        if(rightIdx >= size){
+            if(leftIdx >= size){
+                return;
+            }
+            else{
+                minIdx = leftIdx;
+            }
+        }
+        else {
+            if(heap[leftIdx] <= heap[rightIdx]){
+                minIdx = leftIdx;
+            }
+            else{
+                minIdx = rightIdx;
+            }
+        }
+
+        if(heap[idx] > heap[minIdx]){
+            int tmp = heap[idx];
+            heap[idx] = heap[minIdx];
+            heap[minIdx] = tmp;
+            bubbleDown(minIdx);
+        }
+    }
+
+    public static void main(String[] args){
 
         ReadFile fr = new ReadFile();
 		String file = fr.readTestFile(args[0]);
         ArrayList<Integer> values = fr.tokenizeList(file);
-        head = new ArrayList<Integer>(values.size()*2);
+        heap = new int[values.size()];
+        heapify(values);
+
+        while(size>0){
+            System.out.println(removeMin());
+        }  
       
     }
 }
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import random
 with open("testFile.txt","w+") as fp:
-    values = [i for i in range(0,10001)]
+    values = [i for i in range(0,101)]
     random.shuffle(values)
     for i in values:
         fp.write(str(i)+" ")
